ecf75f9ddce6a3a31341ed1bf763dc5501521266
Ignotus404 <s2002251@sendai-nct.jp>
2024-09-11 02:28:23 +0900

    fix:問題の受信、回答の作成及び回答の送信を一つのコマンドに集約

--- 2024ProconTemporary/Command-Line/BootProgram.cs
+++ 2024ProconTemporary/Command-Line/BootProgram.cs
@@ -3,54 +3,13 @@ using System;
 using _2024ProconTemporary.CommandLine.Commands;
 using _2024ProconTemporary.Base;
 using _2024ProconTemporary.Com;
+using System.Security.Cryptography.X509Certificates;
 
 class BootProgram
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Booting...");
-
-        // 問題データを取得する
-        Console.WriteLine("Getting Problem Data...");
-        HttpClient client = Networking.CreateClient();
-        var problemData = Networking.GetProblemData(client);
-        if (problemData == null)
-        {
-            Console.WriteLine("ERROR: ProblemData Download Failed!");
-            Console.WriteLine("Please check your network connection and try again.");
-            return;
-        }
-        Console.WriteLine("Done!");
-
-
-        // 問題の処理部分
-        // 入力された文字列のコマンドによって処理を分岐する
-        while (true)
-        {
-            Console.WriteLine("Please input command:");
-            string command = Console.ReadLine();
-            if (command == "exit")
-            {
-                break;
-            }
-            /*
-            else if (command == "get")
-            {
-                GetProblemCommandClient getProblemCommandClient = new GetProblemCommandClient();
-                GetProblemCommand getProblemCommand = new GetProblemCommand();
-                getProblemCommand.Handle(getProblemCommandClient);
-            }
-            else if (command == "show")
-            {
-                ShowProblemCommandClient showProblemCommandClient = new ShowProblemCommandClient();
-                ShowProblemCommand showProblemCommand = new ShowProblemCommand();
-                showProblemCommand.Handle(showProblemCommandClient, problemData);
-            }
-            */
-            else
-            {
-                Console.WriteLine("Invalid Command!");
-            }
-        }
+        Cli.Execute<CommandClient>(args);
     }
 }
--- 2024ProconTemporary/Command-Line/CommandClient.cs
+++ 2024ProconTemporary/Command-Line/CommandClient.cs
@@ -7,40 +7,15 @@ namespace _2024ProconTemporary.Base
     public class CommandClient : BaseCliCommands
     {
         [DefaultCommand]
-        [Command("Initialize")]
-        [Help("登録された情報を初期化するコマンド")]
-        public void Initialize(string[] args)
+        [Command("Boot")]
+        [Help("受け取った問題を解くコマンド")]
+        public void Boot(string[] args)
         {
-            var options = Cli.Parse<InitializeCommandClient>(args);
-            var command = new InitializeCommand();
+            var options = Cli.Parse<BootCommandClient>(args);
+            var command = new BootCommand();
             command.Handle(options);
         }
 
-        [Command("GetProblem")]
-        [Help("問題を取得するコマンド")]
-        public void GetProblem(string[] args)
-        {
-            var options = Cli.Parse<GetProblemCommandClient>(args);
-            var command = new GetProblemCommand();
-            command.Handle(options);
-        }
 
-        [Command("EnterAnswerMode")]
-        [Help("解答モードに入るコマンド")]
-        public void EnterAnswerMode(string[] args)
-        {
-            var options = Cli.Parse<EnterAnswerModeCommandClient>(args);
-            var command = new EnterAnswerModeCommand();
-            command.Handle(options);
-        }
-
-        [Command("PostAnswer")]
-        [Help("解答を投稿するコマンド")]
-        public void PostAnswer(string[] args)
-        {
-            var options = Cli.Parse<PostAnswerCommandClient>(args);
-            var command = new PostAnswerCommand();
-            command.Handle(options);
-        }
     }
 }
